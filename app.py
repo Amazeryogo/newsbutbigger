ab63b33b6ba4db82d1d8c652287ddd841e9e80e9
Suvid Datta <suvid.datta@gmail.com>
2021-07-15 21:30:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
-import pyttsx3
-import requests
-import json
 import time
 from flask import *
+import requests
 
 app = Flask(__name__)
 
-url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=SIKE')
+url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=28747323d8ca4c64bc95ac313b7c269c')
 
 
 @app.route('/')
 def frontpage():
-	try:
-	   response = requests.get(url)
-	except:
-	    print("can, t access link, plz check you internet ")
-
+    response = requests.get(url)
     news = json.loads(response.text)
     fontsize = request.args.get("fs")
     if fontsize == None:
-        fontsize = 0
+       fontsize = 0
     print(fontsize)
     return render_template('frontpage.html',news=news,f=fontsize)
