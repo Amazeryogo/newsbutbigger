67fa7245a61179b46d36828ba22c5798a652ff71
Suvid Datta <suvid.datta@gmail.com>
2021-07-15 13:47:27 +0530

    lol

---
+++ app.py
@@ -0,0 +1,27 @@
+# 28747323d8ca4c64bc95ac313b7c269c
+import pyttsx3
+import requests
+import json
+import time
+from flask import *
+
+app = Flask(__name__)
+
+
+
+url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=28747323d8ca4c64bc95ac313b7c269c')
+
+try:
+	response = requests.get(url)
+except:
+	print("can, t access link, plz check you internet ")
+
+news = json.loads(response.text)
+
+@app.route('/')
+def frontpage():
+    fontsize = request.args.get("fs")
+    if fontsize == None:
+        fontsize = 0
+    print(fontsize)
+    return render_template('frontpage.html',news=news,f=fontsize)
