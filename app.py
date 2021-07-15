c142bd9160adb009a025499038d2f2921433f105
Suvid Datta <suvid.datta@gmail.com>
2021-07-15 16:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# 28747323d8ca4c64bc95ac313b7c269c
 import pyttsx3
 import requests
 import json
@@ -7,19 +6,17 @@ from flask import *
 
 app = Flask(__name__)
 
+url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=SIKE')
 
-
-url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=LOLSIKE!')
-
-try:
-	response = requests.get(url)
-except:
-	print("can, t access link, plz check you internet ")
-
-news = json.loads(response.text)
 
 @app.route('/')
 def frontpage():
+	try:
+	   response = requests.get(url)
+	except:
+	    print("can, t access link, plz check you internet ")
+
+    news = json.loads(response.text)
     fontsize = request.args.get("fs")
     if fontsize == None:
         fontsize = 0
