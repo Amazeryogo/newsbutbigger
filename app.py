99a6dca46e38d14006f2508a7efeabf2afc3c68f
Suvid Datta <suvid.datta@gmail.com>
2021-07-31 23:40:37 +0530

    CACHEING v1

--- app.py
+++ app.py
@@ -4,29 +4,59 @@ import requests
 
 app = Flask(__name__)
 
-url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=NO!')
-api_key = "LOL SIKE"
+global refresh_cap
+refresh_cap = 0
+# if this hits 4, we ping newsapi to get us new json
+
+global total
+total = 0
+#total views/refreshes done
+
+global news
+news = ""
+# This is the cache to stop newsapi to brick my apikey ;)
+
+url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=SIKE')
+api_key = "NO"
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+def get_news():
+    response = requests.get(url)
+    newsx = json.loads(response.text)
+    return newsx
 
 @app.route('/')
 def frontpage():
-    response = requests.get(url)
-    news = json.loads(response.text)
+    global refresh_cap
+    refresh_cap = refresh_cap + 1
+    global total
+    total = total + 1
+    if total == 1:
+        global news
+        news = get_news()
+    elif refresh_cap == 4:
+        news = get_news()
+        print(refresh_cap)
+        refresh_cap = 0
+    else:
+        pass
     fontsize = request.args.get("fs")
     if fontsize == None:
        fontsize = 0
-    #print(fontsize)
+
+
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
         ip = request.environ['REMOTE_ADDR']
     else:
         ip = request.environ['HTTP_X_FORWARDED_FOR']
+
+
     hmmm = "https://geolocation-db.com/json/"+ip+"position=true"
     new = requests.get(hmmm).json()
-    city = new['city']
-    complete_url = base_url + "appid=NO"+"&q=" + city
+    city = new['city']  
+    complete_url = base_url + "appid=GETONEYOURSELF"+"&q=" + city
     response = requests.get(complete_url)
     x = response.json()
-    print(x)
     if x["cod"] != "404":
         y = x["main"]
         current_temperature = y["temp"]
@@ -35,3 +65,5 @@ def frontpage():
         z = x["weather"]
         weather_description = z[0]["description"]
     return render_template('frontpage.html',city=city,news=news,f=fontsize,ct=current_temperature,cp=current_pressure,ch=current_humidity,cd=weather_description)
+
+# Suvid Datta 2021
